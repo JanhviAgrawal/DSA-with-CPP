8ed60e7b347ecc2c988d0736fa7595b47728b648
JanhviAgrawal <workjanhvi21@gmail.com>
2025-07-10 15:18:13 +0530

    GitHub

---
+++ Ch-5-Control Structure/5-3/task-6/q1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int a, b, c;
+    cout << "Enter a, b & c;";
+
+    cin >> a >> b >> c;
+
+    if (a < b)
+    {
+        if (a < c)
+        {
+            cout << a << " is minimum" << endl;
+        }
+        else
+        {
+            cout << c << " is minimum" << endl;
+        }
+    }
+    else
+    {
+        if (b < c)
+        {
+            cout << b << " is minimum" << endl;
+        }
+        else
+        {
+            cout << c << " is minimum" << endl;
+        }
+    }
+}
