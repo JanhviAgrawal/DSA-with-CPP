ad733c189afdbe69be5847d22f9669e0a72b4daa
JanhviAgrawal <workjanhvi21@gmail.com>
2025-07-24 01:51:16 +0530

    GitHub

--- ch-7-Patterns with loop/WithoutSpace-HW/p4.cpp
+++ ch-7-Patterns with loop/WithoutSpace-HW/p4.cpp
@@ -5,17 +5,28 @@ main()
 {
     for (int i = 5; i >= 1; i--)
     {
+        int n = 50;
+        int num = 0;
         for (int j = 5; j >= i; j--)
         {
-            if (i % 2 == 0)
+            if (i % 2 != 0)
+            {
+                char ch = j + 64;
+                cout << ch << "\t";
+            }
+            else
             {
                 if (j % 2 != 0)
                 {
-                    cout << "50 ";
+                    cout << (50 - num * 20) << "\t";
+                    n++;
+                }
+                else
+                {
+                    char ch = j + 64;
+                    cout << ch << "\t";
                 }
             }
-            char ch = j + 64;
-            cout << ch << " ";
         }
         cout << endl;
     }
