3c4e54b8784578359ada000018198c0568a3ba01
JanhviAgrawal <workjanhvi21@gmail.com>
2025-11-11 20:15:49 +0530

    Search

--- Ch-24-Searching/linear_searching.cpp
+++ Ch-24-Searching/linear_searching.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 using namespace std;
 
+int searching(int a[], int n)
+{
+    int search_element;
+    cout << endl
+         << "Enter Element to search: ";
+    cin >> search_element;
+
+    int index = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (search_element == a[i])
+        {
+            index = i;
+            break;
+        }
+    }
+    return index;
+}
+
 int main()
 {
     int n;
     cout << "Enter Number of Elements: ";
     cin >> n;
+
+    int a[n];
+
+    cout << "Enter Elements: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter a[" << i << "] : ";
+        cin >> a[i];
+    }
+
+    cout << "Array : ";
+    for (int element : a)
+    {
+        cout << element << " ";
+    }
+
+    int index = searching(a, n);
+    if (index == -1)
+    {
+        cout << endl
+             << "Element not Found.." << endl;
+    }
+    else
+    {
+        cout << "Searched Element Index: " << index << endl;
+    }
 }
